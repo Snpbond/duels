db0ea4cb59964ab5464f2d34d85daefe3025e60b
Snpbond <snpbond@gmail.com>
2013-05-29 20:09:18 -0600

    updated recentduels ordering

--- recentduels.php
+++ recentduels.php
@@ -3,7 +3,7 @@
 require_once('config.php');
 include_once('tf2duel.class.php');
 
-$query = 'select challenger,victim,id from (select challenger,victim,id from duels where processed=1 order by id desc limit 5) as tmp order by id asc';
+$query = 'select challenger,victim,id from (select challenger,victim,id from duels where processed=1 order by id desc limit 5) as tmp order by id desc';
 $res = mysql_query($query) or die(mysql_error());
 
 while($duel = mysql_fetch_assoc($res)){
